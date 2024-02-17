53f4c7f950e2e887a3235b3126068f0951496ff8
RedMagic <lijnbelt13@gmail.com>
2024-02-17 13:19:35 +0100

    Reflections Testing

--- src/main/java/com/redmagic/undefinedapi/extension/JavaPluginExtensions.kt
+++ src/main/java/com/redmagic/undefinedapi/extension/JavaPluginExtensions.kt
@@ -2,8 +2,11 @@ package com.redmagic.undefinedapi.extension
 
 import org.bukkit.plugin.java.JavaPlugin
 import org.reflections.Reflections
+import org.reflections.scanners.FieldAnnotationsScanner
+import org.reflections.scanners.MethodAnnotationsScanner
 import org.reflections.scanners.ResourcesScanner
 import org.reflections.scanners.SubTypesScanner
+import org.reflections.scanners.TypeAnnotationsScanner
 import org.reflections.util.ConfigurationBuilder
 import java.lang.reflect.Method
 
@@ -14,9 +17,8 @@ import java.lang.reflect.Method
  * @return a set of classes annotated with the given annotation
  */
 fun JavaPlugin.findClasses(annotation: Class<out Annotation>): Set<Class<*>> = Reflections(
-    ConfigurationBuilder()
-        .forPackages(this::class.java.packageName)
-        .setScanners(SubTypesScanner(false), ResourcesScanner())
+    this::class.java.packageName,
+    TypeAnnotationsScanner()
 ).getTypesAnnotatedWith(annotation)
 
 /**
@@ -26,7 +28,6 @@ fun JavaPlugin.findClasses(annotation: Class<out Annotation>): Set<Class<*>> = R
  * @return a mutable set of methods annotated with the specified annotation
  */
 fun JavaPlugin.findMethods(annotation: Class<out Annotation>): MutableSet<Method> = Reflections(
-    ConfigurationBuilder()
-        .forPackages(this::class.java.packageName)
-        .setScanners(SubTypesScanner(false), ResourcesScanner())
+    this::class.java.packageName,
+    MethodAnnotationsScanner()
 ).getMethodsAnnotatedWith(annotation)
