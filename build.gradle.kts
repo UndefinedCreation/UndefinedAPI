9954c3e9e94993739238caa39afd54cf8b7192dd
RedMagic <lijnbelt13@gmail.com>
2024-02-20 21:24:07 +0100

    Testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.redmagic"
             artifactId = "UndefinedAPI"
-            version = "0.3.5"
+            version = "0.3.51"
 
             from(components["java"])
         }
@@ -33,7 +33,7 @@ publishing {
 }
 
 group = "com.redmagic"
-version = "0.3.5"
+version = "0.3.51"
 
 repositories {
     mavenCentral()
