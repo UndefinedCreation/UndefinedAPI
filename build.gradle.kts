cfb4cc4734604e40472eef6be2a3e129e9dbeb0e
RedMagic <lijnbelt13@gmail.com>
2024-02-25 15:28:48 +0100

    PlayerHitByPlayerEvent

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.redmagic"
             artifactId = "UndefinedAPI"
-            version = "0.4.02"
+            version = "0.4.03"
 
             from(components["java"])
         }
@@ -33,7 +33,7 @@ publishing {
 }
 
 group = "com.redmagic"
-version = "0.4.02"
+version = "0.4.03"
 
 repositories {
     mavenCentral()
