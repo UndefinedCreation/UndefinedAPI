6917bfd5186a73eba23f04fe2d2032c6e3329e4f
TheRedMagic <lijnbelt13@gmail.com>
2024-05-27 13:12:59 +0200

    PaperWeight Working

--- v1_20_4/build.gradle.kts
+++ v1_20_4/build.gradle.kts
@@ -1,9 +1,8 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import io.github.patrick.gradle.remapper.RemapTask
 
 plugins {
     kotlin("jvm") version "1.9.22"
-    id("io.github.patrick.remapper") version "1.4.2"
+    id("io.papermc.paperweight.userdev") version "1.7.1"
 }
 
 val versionVar = version
@@ -16,7 +15,8 @@ repositories {
 
 
 dependencies {
-    compileOnly("org.spigotmc:spigot:1.20.4-R0.1-SNAPSHOT:remapped-mojang")
+
+    paperweight.paperDevBundle("1.20.4-R0.1-SNAPSHOT")
 
     implementation(project(":api"))
 }
@@ -26,13 +26,7 @@ dependencies {
 tasks {
 
     assemble {
-        dependsOn("remap")
-    }
-
-    remap {
-        version.set("1.20.4")
-        action.set(RemapTask.Action.MOJANG_TO_SPIGOT)
-        archiveName.set("${project.name}-${versionVar}-remapped.jar")
+        dependsOn(reobfJar)
     }
 
     compileKotlin {
