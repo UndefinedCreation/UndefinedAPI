55dea7e2c4de6530d8cc80e1bcf7c09f410673d3
TheRedMagic <lijnbelt13@gmail.com>
2024-05-27 14:11:26 +0200

    Clean

--- common/build/dokka/htmlPartial/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-{"@class":"org.jetbrains.dokka.base.templating.AddToSourcesetDependencies","moduleName":"common","content":{":common:dokkaHtmlPartial/main":[]}}
--- v1_20_4/build/dokka/htmlPartial/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-{"@class":"org.jetbrains.dokka.base.templating.AddToSourcesetDependencies","moduleName":"v1_20_4","content":{":v1_20_4:dokkaHtmlPartial/main":[]}}
